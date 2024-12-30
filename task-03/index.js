ed169bf7eeaab3b4ae6e2b9c973ba1149dc828e2
Iron-56 <nandukrishna.mpm@gail.com>
2024-12-30 15:47:40 +0530

    completed task-03

---
+++ task-03/index.js
@@ -0,0 +1,58 @@
+
+sounds = {}
+
+function add(key, s)
+{
+	sounds[key] = new Audio(`sounds/${s}.mp3`);
+}
+
+function load()
+{
+	add("W", "crash");
+	add("A", "kick-bass");
+	add("S", "snare");
+	add("D", "tom-1");
+	add("J", "tom-2");
+	add("K", "tom-3");
+	add("L", "tom-4");
+}
+
+function addListeners()
+{
+	let buttons = document.querySelector(".set");
+	for(let i = 0; i < buttons.children.length; i++)
+	{
+		buttons.children[i].play = function()
+		{
+			sounds[this.innerHTML].play();
+			this.classList.add("pressed");
+
+			setTimeout(() => {
+				this.classList.remove("pressed");
+			}, 500);
+		}
+
+		buttons.children[i].addEventListener("click", function()
+		{
+			this.play();
+		});
+	}
+
+	document.addEventListener("keydown", function(e)
+	{
+		if(e.key.toUpperCase() in sounds)
+		{
+			document.getElementsByClassName(e.key)[0].play();
+		}
+	});
+}
+
+function main()
+{
+	load();
+	addListeners();
+}
+
+
+
+main();
